bd64c753ffd3f862aefe526a67dbff18be19d2fa
Cody <hoban.j.cody@gmail.com>
2020-04-21 21:36:10 -0400

    App directory

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router> 
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Switch> 
+    </Router>
   );
 }
 
